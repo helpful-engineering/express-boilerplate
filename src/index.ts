0eef2ee35c74120bffaa1f98f8bee7ef1ad58db5
Kyle Kearney <kyledkearney@gmail.com>
2023-02-14 13:11:25 -0700

    setup swagger

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import express, { Express, Request, Response } from 'express';
+import swaggerUi from "swagger-ui-express";
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
-
+app.use(
+    "/docs",
+    swaggerUi.serve,
+    swaggerUi.setup(undefined, {
+        swaggerOptions: {
+            url: "/swagger.json",
+        },
+    })
+);
 
 app.get('/', (req: Request, res: Response) => {
     res.send('express server');
